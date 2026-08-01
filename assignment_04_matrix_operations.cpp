4d0a3e43de441c4edc8a26b28f33d83bb5f5bde7
Franca-81 <faadoma@st.ug.edu.gh>
2026-08-01 11:53:05 +0100

    Implement matrix operations: input, display, transpose, add, multiply

--- assignment_04_matrix_operations.cpp
+++ assignment_04_matrix_operations.cpp
@@ -65,3 +65,106 @@
 #include <string>
 using namespace std;
 
+void inputMatrix(int matrix[10][10], int rows, int cols)
+{
+for (int i = 0; i < rows; i++)
+{
+for (int j = 0; j < cols; j++)
+{
+cin >> matrix[i][j];
+}
+}
+}
+void displayMatrix(int matrix[10][10], int rows, int cols)
+{
+for (int i = 0; i < rows; i++)
+{
+for (int j = 0; j < cols; j++)
+{
+cout << matrix[i][j] << " ";
+}
+cout << endl;
+}
+}
+void transposeMatrix(int matrix[10][10], int rows, int cols)
+{
+int transpose[10][10];
+for (int i = 0; i < rows; i++)
+{
+for (int j = 0; j < cols; j++)
+{
+transpose[j][i] = matrix[i][j];
+}
+}
+cout << "Transposed Matrix:" << endl;
+displayMatrix(transpose, cols, rows);
+}
+void addMatrices(int a[10][10], int b[10][10], int rows, int cols)
+{
+int sum[10][10];
+for (int i = 0; i < rows; i++)
+{
+for (int j = 0; j < cols; j++)
+{
+sum[i][j] = a[i][j] + b[i][j];
+}
+}
+cout << "Sum Matrix:" << endl;
+displayMatrix(sum, rows, cols);
+}
+void multiplyMatrices(int a[10][10], int b[10][10], int m, int n, int p)
+{
+int product[10][10];
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < p; j++)
+{
+product[i][j] = 0;
+for (int k = 0; k < n; k++)
+{
+product[i][j] += a[i][k] * b[k][j];
+}
+}
+}
+cout << "Product Matrix:" << endl;
+displayMatrix(product, m, p);
+}
+int main()
+{
+int matrix[10][10];
+int rows, cols;
+cout << "PART A - Transpose Matrix" << endl;
+cout << "Enter rows and columns: ";
+cin >> rows >> cols;
+cout << "Enter matrix elements:" << endl;
+inputMatrix(matrix, rows, cols);
+transposeMatrix(matrix, rows, cols);
+int a[10][10], b[10][10];
+int m, n;
+cout << "\nPART B - Add Two Matrices" << endl;
+cout << "Enter rows and columns: ";
+cin >> m >> n;
+cout << "Enter first matrix:" << endl;
+inputMatrix(a, m, n);
+cout << "Enter second matrix:" << endl;
+inputMatrix(b, m, n);
+addMatrices(a, b, m, n);
+int r1, c1, r2, c2;
+int matrixA[10][10], matrixB[10][10];
+cout << "\nPART C - Multiply Two Matrices" << endl;
+cout << "Enter rows and columns of Matrix A: ";
+cin >> r1 >> c1;
+cout << "Enter elements of Matrix A:" << endl;
+inputMatrix(matrixA, r1, c1);
+cout << "Enter rows and columns of Matrix B: ";
+cin >> r2 >> c2;
+if (c1 != r2)
+{
+cout << "Matrix multiplication not possible." << endl;
+return 0;
+}
+cout << "Enter elements of Matrix B:" << endl;
+inputMatrix(matrixB, r2, c2);
+multiplyMatrices(matrixA, matrixB, r1, c1, c2);
+return 0;
+}
